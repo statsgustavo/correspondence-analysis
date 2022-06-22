57f493ae29f827f44f57cc6b82a81f2d0777cb48
Luis Gustavo Santos Barros <gustavo.barros@MacBook-Pro-de-Luis.local>
2022-06-22 16:31:39 -0300

    class for encapsulating fixtures for table data and indices.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,36 @@
+from dataclasses import dataclass
+from typing import Union
+
+import numpy as np
 import pandas as pd
 import pytest
 
 
+@dataclass
+class DatasetFixture:
+    row_idx: np.ndarray
+    column_idx: np.ndarray
+    data: Union[pd.DataFrame, np.ndarray]
+
+
 @pytest.fixture(scope="session")
-def table_dataframe():
-    return pd.DataFrame(
+def dataframe():
+    data = pd.DataFrame(
         {
             "None": [4, 4, 25, 18, 10],
             "Light": [2, 3, 10, 24, 6],
             "Medium": [3, 7, 12, 33, 7],
             "Heavy": [2, 4, 4, 13, 2],
         },
-        index = ["SM", "JM", "SE", "JE", "SC"]
+        index=["SM", "JM", "SE", "JE", "SC"],
     )
 
-@pytest.fixture
-def table_array(table_dataframe):
-    return table_dataframe.values
+    return DatasetFixture(data.index.values, data.columns.values, data)
+
+
+@pytest.fixture(scope="session")
+def array(dataframe):
+    data = dataframe.data.values
+    row_idx = np.char.add("R", np.arange(1, data.shape[0] + 1).astype(str))
+    column_idx = np.char.add("C", np.arange(1, data.shape[1] + 1).astype(str))
+    return DatasetFixture(row_idx, column_idx, data)
