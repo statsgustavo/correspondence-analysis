994fda936b42140b64ab81f1094f46288ce0ffca
Luis Gustavo Santos Barros <gustavo.barros@MacBook-Pro-de-Luis.local>
2022-06-23 10:24:09 -0300

    add docstring and refactor code for fixing linting warnings.

--- tests/test_contingency_table.py
+++ tests/test_contingency_table.py
@@ -1,27 +1,34 @@
-import pytest
 from src.contingency_table import ContingencyTable
 
 
 class TestContingencyTable:
+    """Tests for src.contingency.ContingencyTable."""
+
     def test_category_levels_dataframe(self, dataframe):
+        """
+        Tests retrival of pandas.DataFrame contingency table index and column names.
+        """
         table = ContingencyTable(dataframe.data)
-        row, column = table._category_levels()
-        assert (row == dataframe.row_idx).all()
-        assert (column == dataframe.column_idx).all()
+        assert (table.rows.levels == dataframe.row_idx).all()
+        assert (table.columns.levels == dataframe.column_idx).all()
 
     def test_category_levels_array(self, array):
+        """
+        Tests handling of index and column names when a numpy.array is passed as data.
+        """
         table = ContingencyTable(array.data)
-        row, column = table._category_levels()
-        assert (row == array.row_idx).all()
-        assert (column == array.column_idx).all()
+        assert (table.rows.levels == array.row_idx).all()
+        assert (table.columns.levels == array.column_idx).all()
 
     def test_sample_marginal_totals(self, array):
+        """Tests calculation of sample marginal totals."""
         table = ContingencyTable(array.data)
         row, column = table.sample_marginal_totals()
         assert (row == array.data.sum(1)).all()
         assert (column == array.data.sum(0)).all()
 
     def test_sample_marginal_proportions(self, array):
+        """Tests calculation of sample marginal proportions."""
         table = ContingencyTable(array.data)
         proportions = array.data / array.data.sum()
         row, column = table.sample_marginal_proportions()
