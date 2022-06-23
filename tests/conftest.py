c5087e55e80bff0123917b5a4d55d1b917f0d9d8
Luis Gustavo Santos Barros <gustavo.barros@MacBook-Pro-de-Luis.local>
2022-06-23 10:23:33 -0300

    write docstrings and fix linting warning.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,14 +8,17 @@ import pytest
 
 @dataclass
 class DatasetFixture:
+    """Container class for data fixtures."""
+
     row_idx: np.ndarray
     column_idx: np.ndarray
     data: Union[pd.DataFrame, np.ndarray]
 
 
 @pytest.fixture(scope="session")
-def dataframe():
-    data = pd.DataFrame(
+def dataframe() -> DatasetFixture:
+    """Fixture for contingency tables as pandas.DataFrames object."""
+    table = pd.DataFrame(
         {
             "None": [4, 4, 25, 18, 10],
             "Light": [2, 3, 10, 24, 6],
@@ -24,13 +27,20 @@ def dataframe():
         },
         index=["SM", "JM", "SE", "JE", "SC"],
     )
-
-    return DatasetFixture(data.index.values, data.columns.values, data)
+    return DatasetFixture(table.index.values, table.columns.values, table)
 
 
 @pytest.fixture(scope="session")
-def array(dataframe):
-    data = dataframe.data.values
-    row_idx = np.char.add("R", np.arange(1, data.shape[0] + 1).astype(str))
-    column_idx = np.char.add("C", np.arange(1, data.shape[1] + 1).astype(str))
-    return DatasetFixture(row_idx, column_idx, data)
+def array() -> DatasetFixture:
+    """Fixture for contingency tables as numpy.ndarray object."""
+    table = np.array(
+        [
+            [4, 4, 25, 18, 10],
+            [2, 3, 10, 24, 6],
+            [3, 7, 12, 33, 7],
+            [2, 4, 4, 13, 2],
+        ]
+    )
+    row_idx = np.array(["R1", "R2", "R3", "R4"])
+    column_idx = np.array(["C1", "C2", "C3", "C4", "C5"])
+    return DatasetFixture(row_idx, column_idx, table)
