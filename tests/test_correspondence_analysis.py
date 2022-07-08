0adf8106256af24a1effadaaebf6a10aed63a872
Luis Gustavo Santos Barros <gustavo.barros@MacBook-Pro-de-Luis.local>
2022-07-07 22:37:35 -0300

    change fixtures for data from a working paper.

--- tests/test_correspondence_analysis.py
+++ tests/test_correspondence_analysis.py
@@ -14,8 +14,8 @@ class TestCorrespondenceAnalysis:
         assert row.shape == expected_row.shape
         assert column.shape == expected_column.shape
 
-        assert np.allclose(row, expected_row)
-        assert np.allclose(column, expected_column)
+        assert np.allclose(row, expected_row, atol=1e-3)
+        assert np.allclose(column, expected_column, atol=1e-3)
 
     def test_distances(self, array, distances):
         """
@@ -29,8 +29,8 @@ class TestCorrespondenceAnalysis:
         assert row.shape == expected_row.shape
         assert column.shape == expected_column.shape
 
-        assert np.allclose(row, expected_row)
-        assert np.allclose(column, expected_column)
+        assert np.allclose(row, expected_row, atol=1e-4)
+        assert np.allclose(column, expected_column, atol=1e-4)
 
     def test_inertia(self, array, inertia):
         """Tests calculation of row/column profiles inertia."""
@@ -41,8 +41,8 @@ class TestCorrespondenceAnalysis:
         assert row.shape == expected_row.shape
         assert column.shape == expected_column.shape
 
-        assert np.allclose(row, expected_row)
-        assert np.allclose(column, expected_column)
+        assert np.allclose(row, expected_row / expected_row.sum(), atol=1e-2)
+        assert np.allclose(column, expected_column / expected_column.sum(), atol=1e-2)
 
     def test_standardized_residuals_matrix_shape(
         self, array, standardized_residuals_matrix
@@ -59,48 +59,53 @@ class TestCorrespondenceAnalysis:
     ):
         """Tests correctness of standardized residuals matrix calculations."""
         ca = CorrespondenceAnalysis(array.data)
+
         assert np.allclose(
-            ca.standardized_residuals_matrix, standardized_residuals_matrix
+            ca.standardized_residuals_matrix, standardized_residuals_matrix, atol=1e-4
         )
 
     def test_factor_scores_shape(self, array, factor_scores):
         """Test factor scores matrices shapes."""
         row_expected, column_expected = factor_scores
         ca = CorrespondenceAnalysis(array.data)
-        assert ca.profiles.row.factor_scores.shape == row_expected.shape
-        assert ca.profiles.column.factor_scores.shape == column_expected.shape
+        assert ca.profiles.row.factor_scores.shape[0] == row_expected.shape[0]
+        assert ca.profiles.column.factor_scores.shape[0] == column_expected.shape[0]
 
     def test_factor_scores_values(self, array, factor_scores):
         """Test correctness of factor scores matrices values."""
         row_expected, column_expected = factor_scores
         ca = CorrespondenceAnalysis(array.data)
-        assert np.allclose(ca.profiles.row.factor_scores, row_expected, atol=1e-7)
-        assert np.allclose(ca.profiles.column.factor_scores, column_expected, atol=1e-7)
+        assert np.allclose(
+            ca.profiles.row.factor_scores[:, :2], row_expected, atol=1e-3
+        )
+        assert np.allclose(
+            ca.profiles.column.factor_scores[:, :2], column_expected, atol=1e-3
+        )
 
     def test_profile_correlation_shape(self, array, profile_correlation):
         """Test profile correlation matrices shapes."""
         row_expected, column_expected = profile_correlation
         ca = CorrespondenceAnalysis(array.data)
-        assert ca.profiles.row.cor.shape == row_expected.shape
-        assert ca.profiles.column.cor.shape == column_expected.shape
+        assert ca.profiles.row.cor.shape[0] == row_expected.shape[0]
+        assert ca.profiles.column.cor.shape[0] == column_expected.shape[0]
 
     def test_profile_correlation_values(self, array, profile_correlation):
         """Test correctness of profile correlation values."""
         row_expected, column_expected = profile_correlation
         ca = CorrespondenceAnalysis(array.data)
-        assert np.allclose(ca.profiles.row.cor, row_expected, atol=1e-7)
-        assert np.allclose(ca.profiles.column.cor, column_expected, atol=1e-7)
+        assert np.allclose(ca.profiles.row.cor[:, :2], row_expected, atol=1e-3)
+        assert np.allclose(ca.profiles.column.cor[:, :2], column_expected, atol=1e-3)
 
     def test_profile_contribution_shape(self, array, profile_contribution):
         """Test profile contribution matrices shapes."""
         row_expected, column_expected = profile_contribution
         ca = CorrespondenceAnalysis(array.data)
-        assert ca.profiles.row.ctr.shape == row_expected.shape
-        assert ca.profiles.column.ctr.shape == column_expected.shape
+        assert ca.profiles.row.ctr.shape[0] == row_expected.shape[0]
+        assert ca.profiles.column.ctr.shape[0] == column_expected.shape[0]
 
     def test_profile_contribution_values(self, array, profile_contribution):
         """Test correctness of profile contribution values."""
         row_expected, column_expected = profile_contribution
         ca = CorrespondenceAnalysis(array.data)
-        assert np.allclose(ca.profiles.row.ctr, row_expected, atol=1e-7)
-        assert np.allclose(ca.profiles.column.ctr, column_expected, atol=1e-7)
+        assert np.allclose(ca.profiles.row.ctr[:, :2], row_expected, atol=1e-3)
+        assert np.allclose(ca.profiles.column.ctr[:, :2], column_expected, atol=1e-3)
