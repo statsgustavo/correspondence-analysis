949fd442ea5b380fe88ce94972ea2f352a7e959a
statsgustavo <stats.gustavo@gmail.com>
2022-07-12 16:12:37 -0300

    refactor summary and plot creation.

--- src/correspondence_analysis.py
+++ src/correspondence_analysis.py
@@ -76,6 +76,7 @@ class CorrespondenceAnalysis(BaseCorrespondenceAnalysis):
 
         self.profiles = CorrespondenceAnalysisResults(
             self._set_profile(
+                self.rows.levels,
                 row_mass,
                 row_weights,
                 row_distance,
@@ -85,6 +86,7 @@ class CorrespondenceAnalysis(BaseCorrespondenceAnalysis):
                 row_ctr,
             ),
             self._set_profile(
+                self.columns.levels,
                 column_mass,
                 column_weights,
                 column_distance,
@@ -96,20 +98,20 @@ class CorrespondenceAnalysis(BaseCorrespondenceAnalysis):
         )
 
         (
-            self.eigenvalues_summary,
+            self.inertia_summary,
             self.rows_summary,
             self.columns_summary,
         ) = self._summary_tables()
 
     def _set_profile(
-        self, mass, weights, distances, inertia, scores, correlation, contribution
+        self, name, mass, weight, distance, inertia, score, correlation, contribution
     ):
         """
         Creates instance of OneDimentionResults object with the corresponding row/column
         profiles statistics.
         """
         return OneDimensionResults(
-            mass, weights, distances, inertia, scores, correlation, contribution
+            name, mass, weight, distance, inertia, score, correlation, contribution
         )
 
     def _standardized_residuals_matrix(
@@ -157,57 +159,71 @@ class CorrespondenceAnalysis(BaseCorrespondenceAnalysis):
         """
         return mass * np.square(factors) / eigenvalues.T
 
-    def _angle(self):
-        """Angle between the axis and the profile."""
+    def _profile_summary(self, profiles: OneDimensionResults) -> pd.DataFrame:
+        """
+        Build row/column profile summary as a pandas.DataFrame.
+
+        :param profiles: OneDimensionResults object for either rows or columns.
+        :returns table: pandas.DataFrame summary with mass, inertia, score, contribution
+        and correlation for each row/column profile.
+        """
+        column_names = ["Mass", "Inertia", "F1", "F2", "CTR1", "CTR2", "COR1", "COR2"]
+        table = pd.DataFrame(
+            np.column_stack(
+                [
+                    profiles.mass,
+                    profiles.inertia,
+                    profiles.factor_score[:, :2],
+                    profiles.ctr[:, :2],
+                    profiles.cor[:, :2],
+                ]
+            ),
+            index=profiles.name.ravel(),
+            columns=column_names,
+        )
+        return table
+
+    def _inertia_summary(self):
+        """
+        Summary table of inertia for the first two dimensions of row and column
+        profiles. Since rows and columns share the same eigenvalues, the results are
+        exactly the same for the dimensions of rows or columns.
+
+        :returns table: pandas.DataFrame summary with inertia (eigenvalue), chi-square,
+        percentage of explained variance and its cumulative value.
+        """
+        column_names = [
+            "Inertia",
+            "Chi-square",
+            "Percent",
+            "Cumulative",
+        ]
+
+        n = self.table.sum()
+        eigenvalues = self.eigenvalues.ravel()[:2]
+
+        table = pd.DataFrame(
+            np.column_stack(
+                [
+                    eigenvalues,
+                    n * eigenvalues,
+                    eigenvalues / self.total_inertia,
+                    eigenvalues.cumsum() / self.total_inertia,
+                ]
+            ),
+            index=["Dimension 1", "Dimension 2"],
+            columns=column_names,
+        )
+        totals = pd.DataFrame(
+            table.sum(0).values, columns=["Total"], index=column_names
+        )
+        return pd.concat([table, totals.T], axis=0)
 
     def _summary_tables(self):
         eigenvalues, rows, columns = (
-            pd.DataFrame(
-                {
-                    "Inertia": self.eigenvalues.ravel()[:2],
-                    "Chi-square": self.table.sum() * self.eigenvalues.ravel()[:2],
-                    "Percent": self.eigenvalues.ravel()[:2] / self.total_inertia,
-                    "Cumulative": self.eigenvalues.ravel().cumsum()[:2]
-                    / self.total_inertia,
-                },
-                index=["Dimension 1", "Dimension 2"],
-            ).pipe(
-                lambda d: pd.concat(
-                    [
-                        d,
-                        pd.DataFrame(
-                            d.sum(0).values, columns=["Total"], index=d.columns
-                        ).T,
-                    ],
-                    axis=0,
-                )
-            ),
-            pd.DataFrame(
-                np.column_stack(
-                    [
-                        self.profiles.row.mass,
-                        self.profiles.row.inertia,
-                        self.profiles.row.factor_scores[:, :2],
-                        self.profiles.row.ctr[:, :2],
-                        self.profiles.row.cor[:, :2],
-                    ]
-                ),
-                index=self.rows.levels.ravel(),
-                columns=["Mass", "Inertia", "F1", "F2", "CTR1", "CTR2", "COR1", "COR2"],
-            ),
-            pd.DataFrame(
-                np.column_stack(
-                    [
-                        self.profiles.column.mass,
-                        self.profiles.column.inertia,
-                        self.profiles.column.factor_scores[:, :2],
-                        self.profiles.column.ctr[:, :2],
-                        self.profiles.column.cor[:, :2],
-                    ]
-                ),
-                index=self.columns.levels.ravel(),
-                columns=["Mass", "Inertia", "G1", "G2", "CTR1", "CTR2", "COR1", "COR2"],
-            ),
+            self._inertia_summary(),
+            self._profile_summary(self.profiles.row),
+            self._profile_summary(self.profiles.column),
         )
         return eigenvalues, rows, columns
 
@@ -215,7 +231,7 @@ class CorrespondenceAnalysis(BaseCorrespondenceAnalysis):
         """Shows summary tables of the correspondence analysis."""
         report = reports.Report(
             [
-                reports.Table("Eigenvalues", self.eigenvalues_summary.round(precision)),
+                reports.Table("Inertia", self.inertia_summary.round(precision)),
                 reports.Table("Row profiles", self.rows_summary.round(precision)),
                 reports.Table("Column profiles", self.columns_summary.round(precision)),
             ]
@@ -229,11 +245,5 @@ class CorrespondenceAnalysis(BaseCorrespondenceAnalysis):
         """
         _ = plt.figure(figsize=(10, 10))
         splot = plt.subplot(111)
-        splot = plot_profile_coordiantes(
-            self.profiles.row,
-            self.profiles.column,
-            self.rows.levels,
-            self.columns.levels,
-            splot,
-        )
+        splot = plot_profile_coordiantes(self.profiles.row, self.profiles.column, splot)
         return splot
