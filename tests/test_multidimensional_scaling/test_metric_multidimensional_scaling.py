261bb58d9a9193503b13d0d5d86c186f1c0ae9b5
statsgustavo <stats.gustavo@gmail.com>
2022-07-14 16:43:49 -0300

    add docstring

--- tests/test_multidimensional_scaling/test_metric_multidimensional_scaling.py
+++ tests/test_multidimensional_scaling/test_metric_multidimensional_scaling.py
@@ -7,10 +7,16 @@ class TestMetricMultidimensionalScaling:
     """Tests for metric multidimensional scaling implementation."""
 
     def test_get_distance_metric_function_raises_error(self):
+        """
+        Tests if metric validation raises error when invalid metric is passed.
+        """
         with pytest.raises(InvalidMetricError):
             _ = mms.get_distance_metric_function("speed")
 
     def test_get_distance_metric_function_returns_function(self):
+        """
+        Tests if object returned by metric validation is a function.
+        """
         for m in ["euclidean", "l1", "l2", "cosine"]:
             fn = mms.get_distance_metric_function(m)
             assert callable(fn)
