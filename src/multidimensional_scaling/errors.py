f08a4fc369b707ab023d8f5b6d59380e28826c71
statsgustavo <stats.gustavo@gmail.com>
2022-07-15 09:46:03 -0300

    exception for when metric is not valid.

--- src/multidimensional_scaling/errors.py
+++ src/multidimensional_scaling/errors.py
@@ -1,17 +1,14 @@
-from typing import Any
-
-from sklearn.metrics import pairwise
+from typing import Any, List
 
 
 class InvalidMetricError(Exception):
     """Exception for invalid distance metrics."""
 
-    VALID_METRICS = list(pairwise.PAIRED_DISTANCES.keys())
-
-    def __init__(self, value: Any):
+    def __init__(self, value: Any, expected: List[str]):
         self.value = value
+        self._expected = expected
         self.message = (
-            "Invalid distance metric. Expected one of {VALID_METRICS}"
+            "Invalid distance metric. Expected one of {self._expected}"
             + "by got `{self.value}`."
         )
         super().__init__(self.message)
